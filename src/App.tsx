7bff05131953e9ab5bb0a12ce29a9c75420c1af6
sangavi002 <sangavibabu007@gmail.com>
2024-11-18 23:43:15 +0530

    Implement dynamic-form-generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import JSONEditor from './components/JSONEditor';
+import FormPreview from './components/FormPreview';
+import './index.css';
 
-function App() {
+const App: React.FC = () => {
+  const [form, setForm] = useState<any>(null);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleUpdate = (updatedSchema: any) => {
+    setForm(updatedSchema);
+  };
+
+  const copyFormJSON = () => {
+    if (form) {
+      navigator.clipboard.writeText(JSON.stringify(form, null, 2));
+      alert('Form JSON copied to clipboard!');
+    } else {
+      alert('No JSON schema to copy.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${darkMode ? 'dark' : ''} `}>
+      <div className=" h-5/6 flex flex-col md:flex-row bg-white dark:bg-gray-900 text-black dark:text-white">
+        <div className=" w-full md:w-1/2 h-1/2 md:h-full bg-gray-100 dark:bg-gray-800 p-4">
+          <div className="flex justify-between items-center">
+            <h1 className="text-2xl font-bold mb-4">JSON Schema Editor</h1>
+            <button
+              onClick={() => setDarkMode(!darkMode)}
+              className="mb-4 px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-500"
+            >
+              {darkMode ? 'Light' : 'Dark'} Mode
+            </button>
+          </div>
+          <div className="flex">
+            <button
+              onClick={copyFormJSON}
+              className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-900"
+            >
+              Copy Form JSON
+            </button>
+          </div>
+          <JSONEditor onUpdate={handleUpdate} />
+        </div>
+
+        <div className="w-full md:w-1/2 h-1/2 md:h-full bg-white dark:bg-gray-900 p-4 overflow-auto">
+          <h1 className="text-2xl font-bold mb-4">Form Preview</h1>
+          {form ? (
+            <FormPreview schema={form} />
+          ) : (
+            <p className="text-gray-500 dark:text-gray-400">Edit the JSON schema to preview the form.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
